da32cc36e06930042ed20d665155b42062299390
Abdirahem <elskumar.am@gmail.com>
2024-05-06 17:32:29 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "./stepper.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function Stepper() {
+  const steps = ["Customer", "Shippment", "Payment"];
+  const [currentStep, setCurrentStep] = useState(1);
+  const [complete, setComplete] = useState(false);
+
+  return (
+    <>
+      <div className="w-2/6  mt-40 m-auto flex justify-between">
+        {steps.map((step, i) => (
+          <div
+            key={i}
+            className={`step-item ${currentStep === i + 1 && "active"} ${
+              (i + 1 < currentStep || complete) && "complete"
+            }`}
+          >
+            <div className="step">{i + 1}</div>
+            <p className="text-gray-500">{step}</p>
+          </div>
+        ))}
+      </div>
+      <div className="w-1/6 mt-10 m-auto">
+        <button
+          className="btn"
+          onClick={() => {
+            currentStep === steps.length
+              ? setComplete(true)
+              : setCurrentStep((prev) => prev + 1);
+          }}
+        >
+          Next {currentStep}
+        </button>
+      </div>
+    </>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Stepper />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
